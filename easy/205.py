093efbecfd26f6bdd0166ae1587b1fbe3883ab1a
Vasanth Vanan <30904627+VasanthVanan@users.noreply.github.com>
2024-01-20 11:32:09 -0500

    Update 205.py

--- easy/205.py
+++ easy/205.py
@@ -6,14 +6,22 @@ def function(inputs):
     str_2 = inputs[1]
     letter_hash = {}
 
+    if(len(str_1)!= len(str_2)):
+        return False
+    
+    if(len(str_1) <= 1 or len(str_2) <= 1):
+        return True
+
     # iterate through the strings
     for i in range(len(str_1)):
+
+        # check the hash value is not equal to the current character
+        if((str_1[i] in letter_hash and letter_hash[str_1[i]] != str_2[i]) or (str_1[i] == str_2[i])):
+            return False
+        
         # check if the character is not present in the hash
         if(str_1[i] not in letter_hash):
             letter_hash[str_1[i]] = str_2[i]
-        # check the hash value is not equal to the current character
-        elif(str_1[i] in letter_hash and letter_hash[str_1[i]] != str_2[i]):
-            return False
 
     return True
 
@@ -32,5 +40,10 @@ assert function(input_2) == expected_output_2, f"Test Case 2 Failed"
 input_3 = ["paper", "title"]
 expected_output_3 = True
 assert function(input_3) == expected_output_3, f"Test Case 3 Failed"
+
+# Test Case 4
+input_4 = ["badc", "baba"]
+expected_output_4 = False
+assert function(input_4) == expected_output_4, f"Test Case 4 Failed"
 
 print("All test cases passed!")
